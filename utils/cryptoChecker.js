a48cbd56419d96233cfc603b311c2ee134f382cf
Georgy <cyberninja2007@duck.com>
2025-02-17 12:48:21 +0300

    Для расходов сделал авто-чек

--- utils/cryptoChecker.js
+++ utils/cryptoChecker.js
@@ -3,6 +3,9 @@ import {getCryptoTransactions} from "../db/controllers/CryptoTransactionControll
 const ACCEPTABLE_DIFFERENCE = 1;
 
 export const checkCrypto = async (transaction) => {
+    if(transaction.hash  === "")
+        return true;
+
     const cryptoTransactions = await getCryptoTransactions(transaction.hash);
 
     let cryptoTransactionsSum = 0;
